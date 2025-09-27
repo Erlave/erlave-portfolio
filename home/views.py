eb9a4078f21e29e3c91892daaeace747dd00dff2
Erlave <erlave.work@gmail.com>
2025-09-27 18:22:43 +0330

    load static & use render partial

--- home/views.py
+++ home/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def home_view(request):
     return render (request , 'homes/home_page.html')
+
+
+
+
+def footer_componnent(request):
+    return render(request,'footer_componnent.html')
+
+def header_componnent(request):
+    return render(request,'header_componnent.html')
+
+
+
